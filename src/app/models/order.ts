cd25a4f008b18b805c5b3553c4562c97bad8e680
Vetrivel <adb6026@usftechs-MacBook-Pro-3.local>
2019-08-01 23:04:11 -0500

    adding model

---
+++ src/app/models/order.ts
@@ -0,0 +1,11 @@
+export interface Order {
+    orderNumber: string;
+    poNumber: string;
+    totalCase: String;
+    totalEach: String;
+    submitted: string;
+    estimatedDelivery: String;
+    status: String;
+}
+
+export default Order;
